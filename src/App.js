98017f492ffdcc55015edae231ed3959d24152a9
majindesu <rwroguesaint@gmail.com>
2023-10-19 00:46:21 +0700

    Updated App.js to include Parallax

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Navigation from './components/Navigation'
 import Header from './components/Header'
 import Intro from './components/Intro'
 import Features from './components/Features'
+import Team from "./components/Team";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+
 const App = () => {
     return(
-            <div className="App">
-                <Navigation/>
-                <Header/>
-                <Intro/>
-                <Features/>
-            </div>
+        <div className="App">
+            <Navigation/>
+            <Parallax className="appAnimation"
+            pages={2} 
+            style={{ top: '0', left: '0' }}>
+                <ParallaxLayer 
+                offset={0} 
+                speed={1}>
+                    <div className='appBackdrop parallax'>
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer
+                offset={0}
+                speed={1.5}>
+                    <Header/>
+                    <Intro/>
+                    <Features/>
+                    <Team/>
+                </ParallaxLayer>
+
+            </Parallax>
+
+
+        </div>
     );
 }
 
 export default App;
+
+
+
+
+
