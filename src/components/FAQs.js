77a000c26a595db59e922e1fb491af62140fce2d
majindesu <rwroguesaint@gmail.com>
2023-10-27 20:26:10 +0700

    added graphics to FAQ

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Card, 
         CardBody,
         CardText, 
+        CardImg,
         CardTitle, 
         CardSubtitle,
-        CardGroup, 
+        CardFooter, 
         Col,
         Row,
         Button} 
 from "reactstrap";
 import "./FAQStyles.css";
-import review from '../assets/reviews.svg'
-
+import komunitas from '../assets/community.png'
+import langganan from '../assets/payment.png'
+import privasi from '../assets/privacy.png'
+import penggunaan from '../assets/penggunaan.png'
 const FAQs = () => {
     return(
         <>
@@ -22,61 +25,89 @@ const FAQs = () => {
         <Row tag="div" className="faqrow">    
             <Col sm="3">
                 <Card body className="faqCard" tag="div">
+                        <CardImg
+                            alt="Penggunaan Aplikasi"
+                            src={penggunaan}
+                            top
+                            width="100%"/>
                         <CardTitle tag="h5">
                         Penggunaan Aplikasi
                         </CardTitle>
-                        <CardSubtitle className="mb-2 text-muted"
+                        <CardSubtitle className="faqsubtitle mb-2 text-muted"
                                     tag="h6">
                         Bantuan seputar fitur, konektivitas dan 
                         penggunaan aplikasi secara umum
                         </CardSubtitle>
-                        <Button>
-                        Baca Selengkapnya...
-                        </Button>
                 </Card>
+                <CardFooter>
+                            <Button className="faqbutton align-self-end">
+                                Baca Selengkapnya...
+                            </Button>
+                </CardFooter>
             </Col>
             <Col sm="3">
                 <Card body className="faqCard" tag="div">
+                        <CardImg
+                            alt="Akun dan Privasi"
+                            src={privasi}
+                            top
+                            width="100%"/>
                         <CardTitle tag="h5">
                         Akun dan Privasi
                         </CardTitle>
-                        <CardSubtitle className="mb-2 text-muted"
+                        <CardSubtitle className="faqsubtitle mb-2 text-muted"
                                     tag="h6">
                         Bantuan seputar pemulihan akun, pengaturan privasi, penggunaan 
                         data pribadi, dsb.
                         </CardSubtitle>
-                        <Button>
-                        Baca Selengkapnya...
-                        </Button>
                 </Card>
+                <CardFooter>
+                            <Button className="faqbutton align-self-end">
+                                Baca Selengkapnya...
+                            </Button>
+                </CardFooter>
             </Col>
             <Col sm="3">
                 <Card body className="faqCard" tag="div">
+                    <CardImg
+                            alt="Langganan Premium"
+                            src={langganan}
+                            top
+                            width="100%"/>
                     <CardTitle tag="h5">
                         Langganan Premium
                         </CardTitle>
-                        <CardSubtitle className="mb-2 text-muted"
+                        <CardSubtitle className="faqsubtitle mb-2 text-muted"
                                     tag="h6">
                         Bantuan seputar pembayaran dan/atau langganan premium.
                         </CardSubtitle>
-                        <Button>
-                        Baca Selengkapnya...
-                        </Button>
                 </Card>
+                <CardFooter>
+                            <Button className="faqbutton align-self-end">
+                                Baca Selengkapnya...
+                            </Button>
+                </CardFooter>
             </Col>
             <Col sm="3">
                 <Card body className="faqCard" tag="div">
+                    <CardImg
+                            alt="Komunitas"
+                            src={komunitas}
+                            top
+                            width="100%"/>
                     <CardTitle tag="h5">
                         Komunitas
                         </CardTitle>
-                        <CardSubtitle className="mb-2 text-muted"
+                        <CardSubtitle className="faqsubtitle mb-2 text-muted"
                                     tag="h6">
                         Bantuan seputar dukungan komunitas, kolaborasi, dan pengadaan event.
                         </CardSubtitle>
-                        <Button>
-                        Baca Selengkapnya...
-                        </Button>
                 </Card>
+                <CardFooter>
+                            <Button className="faqbutton align-self-end">
+                                Baca Selengkapnya...
+                            </Button>
+                </CardFooter>
             </Col>
             </Row>
         </div>
