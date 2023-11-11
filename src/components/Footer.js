b8b32e2ade347a7dad200b5b51b5f28e053fe3be
kaisar <kaisar0354@gmail.com>
2023-11-11 08:52:32 +0700

    fixing responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,15 @@ const Footer = () => {
   return (
     <div className='footerCont'>
         <footer>
-          <Row className='footerRow' tag="div">
-          <Col xs="4"
+          <div className='footerRow' >
+          <div
           className='footerCol'>
             <div className='footerDivLeft'>
               <h3>ReduSave</h3>
               <h6>App by <a href="https://www.linkedin.com/in/eco-route/" class="text-dark fw-bold" >Eco-route</a></h6>
             </div>
-          </Col>
-          <Col xs="4"
+          </div>
+          <div
           className='footerCol'>
             <div className='footerDivMid'>
               <h3>Site Links</h3>
@@ -23,8 +23,8 @@ const Footer = () => {
               <h6>User Reviews</h6>
               <h6>Frequently Asked Questions</h6>
             </div>
-          </Col>
-          <Col xs="4"
+          </div>
+          <div
           className='footerCol'>
             <div className='footerDivRight'>
               <h3>Contacts</h3>
@@ -34,8 +34,8 @@ const Footer = () => {
               <h6> Instagram </h6>
               <h6> Twitter </h6>
             </div>
-          </Col>
-          </Row>
+          </div>
+          </div>
         </footer>
     </div>
   )
