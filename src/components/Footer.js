c916ba4bb0dace643a07aa8f69153309c3799123
majindesu <rwroguesaint@gmail.com>
2023-12-07 16:27:20 +0700

    changed features page, changed layout for landing, added betatester form link to button, fixed footer a bit.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import {Col, Row} from 'reactstrap'
-import './FooterStyles.css'
+import React from "react";
+import { Col, Row } from "reactstrap";
+import "./FooterStyles.css";
+import Logo from "../assets/Logo - White.svg";
 
 const Footer = () => {
   return (
-    <div className='footerCont'>
-    
-        <footer>
-        
-          <div className='footerRow' >
-          <div
-          className='footerCol'>
-            <div className='footerDivLeft'>
-              <h3>ReduSave</h3>
-              <h6>App by <a href="https://www.linkedin.com/in/eco-route/" class="text-dark fw-bold" >Eco-route</a></h6>
+    <div className="footerCont">
+      <footer>
+        <div className="footerRow">
+          <div className="footerCol">
+            <div className="footerDivLeft">
+              <a
+                href="https://www.linkedin.com/in/eco-route/"
+                class="text-dark fw-bold"
+              >
+                <img src={Logo} />
+              </a>
             </div>
           </div>
-          <div
-          className='footerCol'>
-            <div className='footerDivMid'>
+          <div className="footerCol">
+            <div className="footerDivMid">
               <h3>Site Links</h3>
               <h6>News and Notices</h6>
               <h6>App Features</h6>
@@ -26,21 +27,28 @@ const Footer = () => {
               <h6>Frequently Asked Questions</h6>
             </div>
           </div>
-          <div
-          className='footerCol'>
-            <div className='footerDivRight'>
+          <div className="footerCol">
+            <div className="footerDivRight">
               <h3>Contacts</h3>
               <h6> Email </h6>
-              <a href='http://wa.me/6285236944692'> Whatsapp </a>
+              <a href="http://wa.me/6285236944692"> 
+              <h6>Whatsapp</h6>
+              </a>
+              <a href="https://www.linkedin.com/in/eco-route/">
               <h6> LinkedIn </h6>
+              </a>
+              <a href="https://www.instagram.com/ecoroute.apps">
               <h6> Instagram </h6>
+              </a>
+              <a href="https://www.x.com/ecoroute_apps">
               <h6> Twitter </h6>
+              </a>
             </div>
           </div>
-          </div>
-        </footer>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
