53f466dcff8f6cbeac0908fee7c6acd7a35a7bef
kaisar <kaisar0354@gmail.com>
2024-01-23 20:50:14 +0700

    add new team photo

--- src/components/Team.js
+++ src/components/Team.js
@@ -65,33 +65,33 @@ const Team = () => {
         </div>
         <Row>
         <Col xs="3"></Col>
-        <Col className="member" id="hakam" xs="6">
-            <img className="photo" src={require("../assets/placeholder.jpg")} />
+        <Col className="member" data-aos="fade-up" data-aos-duration="1000"  id="hakam" xs="6">
+            <img className="photo" src={require("../assets/hakam.PNG")} />
             <h1>Muhammad Hakam F.</h1>
             <h2>Mobile Developer</h2>
           </Col>
           <Col xs="3"></Col>
         </Row>
         <Row>
-          <Col className="member" id="niken" xs="6">
-            <img className="photo" src={require("../assets/placeholder.jpg")} />
+          <Col className="member" data-aos="fade-up" data-aos-duration="1500"  id="niken" xs="6">
+            <img className="photo" src={require("../assets/niken.JPG")} />
             <h1>Niken Larasati</h1>
             <h2>Machine Learning Engineer</h2>
           </Col>
-          <Col className="member" id="lintang" xs="6">
-            <img className="photo" src={require("../assets/placeholder.jpg")} />
+          <Col className="member" data-aos="fade-up" data-aos-duration="1500"  id="lintang" xs="6">
+            <img className="photo" src={require("../assets/elma.JPEG")} />
             <h1>Lintang Pramayasti</h1>
             <h2>Machine Learning Engineer</h2>
           </Col>
         </Row>
         <Row>
-          <Col className="member" id="aman" xs="6">
-            <img className="photo" src={require("../assets/placeholder.jpg")} />
+          <Col className="member" data-aos="fade-up" data-aos-duration="2000"  id="aman" xs="6">
+            <img className="photo" src={require("../assets/nurkholis.JPG")} />
             <h1>Nurkholis Amanullah</h1>
             <h2>Cloud Computing Engineer</h2>
           </Col>
-          <Col className="member" id="kaisar" xs="6">
-            <img className="photo" src={require("../assets/placeholder.jpg")} />
+          <Col className="member" data-aos="fade-up" data-aos-duration="2000"  id="kaisar" xs="6">
+            <img className="photo" src={require("../assets/kaisar.jpg")} />
             <h1>Kaisar Fauzan</h1>
             <h2>Cloud Computing Engineer</h2>
           </Col>
