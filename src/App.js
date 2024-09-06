35fad9a4baf2780de382920b8e4a98519b584c56
unknown <kaisar0354@gmail.com>
2024-09-06 17:25:18 +0700

    fix all responsive

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
             <CarbonEmission/>
             <Teams/>
             <FAQPage/>
-            <DownloadPage />
+            {/*<DownloadPage />*/}
             <Footer/>
             
         </div>
