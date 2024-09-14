a8675919f70512ae498ec7c05f26525cd2b84f54
unknown <kaisar0354@gmail.com>
2024-09-14 08:13:23 +0700

    fix all responsive

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -10,9 +10,9 @@ const Introduction = ()=>{
                 <h1 className="firstTitle">great steps for a</h1>
                 <h1 className="secondTitle">green earth</h1>
                 <p>Download the Eco-Route app from the Play Store and App Store, create an account using your vehicle information, and embark on your journey. Take small steps to kickstart big changes.</p>
-                <a href="https://bit.ly/UnduhEcoRoute">
+                {/* <a href="https://bit.ly/UnduhEcoRoute">
                     <img  src={googleplay} alt="google-play-logo"></img>
-                </a>
+                </a> */}
             </div>
             <div className="rightSide">
                 <img className="introImg" src={bumi} alt="bumi" ></img>
